4ddb88c8f12d69220f27b143a80b79fa33132a03
Hamish Charles <hamish.charles@so.energy>
2019-10-11 17:32:06 +0100

    Add some basic JS event structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,26 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+let menuButtonClick = function(e) {
+  console.log("menu button clicked", e.target, e.target.nextsibling);
+};
+
+let menuItemClick = function(e) {
+  console.log("clicked", e.target);
+};
+
+let setUpMenuButtons = function() {
+  let menuButtons = document.querySelectorAll("nav button[aria-haspopup=true]");
+  menuButtons.forEach(menuItem =>
+    menuItem.addEventListener("click", menuButtonClick)
+  );
+};
+
+let setUpMenuItems = function() {
+  let menuItems = document.querySelectorAll("[role=link], [role=menuitem]");
+  menuItems.forEach(menuItem =>
+    menuItem.addEventListener("click", menuItemClick)
+  );
+};
+
+setUpMenuButtons();
+setUpMenuItems();
